46a0c670c9b237bfd28018886edf2fcf4b490781
Mark Pang <30737313+Pairallax@users.noreply.github.com>
2019-10-12 22:22:47 -0700

    Setup server + Implement User Interface

---
+++ API/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    balance: {
+        type: Number,
+        default: 0
+    },
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
