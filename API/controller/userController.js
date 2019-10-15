e2165a923e78ca7a6cac930771de4c3719c983bf
Victoria Provenzano <victoriaprovenzano@gmail.com>
2019-10-15 15:29:28 -0700

    first commit victoria

--- API/controller/userController.js
+++ API/controller/userController.js
@@ -3,18 +3,21 @@ const router = express.Router();
 const userService = require('../service/userService');
 
 router.get('/users/:email', function (req, res, next) {
+    console.log('in here 1');
     userService.getUserByEmail(req.params.email, res).then(function (user) {
         res.send(user);
     }).catch(next);
 });
 
 router.post('/users', function (req, res, next) {
+    console.log('in here 2');
     userService.addUser(req.body).then(function (user) {
         res.send(user);
     }).catch(next);
 });
 
 router.put('/users/:email', function (req, res, next) {
+    console.log('in here 3');
     userService.updateUser(req.params.email, req.body).then(function () {
         //  need to return the UPDATED user, not the found one
         userService.getUserByEmail(req.params.email).then(function(updatedUser){
@@ -24,6 +27,7 @@ router.put('/users/:email', function (req, res, next) {
 });
 
 router.delete('/users/:email', function (req, res, next) {
+    console.log('in here 4');
     userService.deleteUser(req.params.email).then(function (user) {
         res.send(user);
     }).catch(next);
