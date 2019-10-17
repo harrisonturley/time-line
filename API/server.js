65e9b3ba8a455986aa5f5ee630a3b216afa5070d
Victoria Provenzano <victoriaprovenzano@gmail.com>
2019-10-17 14:55:32 -0700

    fixing more bugs in notificationController

--- API/server.js
+++ API/server.js
@@ -2,26 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const docs = require("express-mongoose-docs");
-//var admin = require('firebase-admin');
 
 // mongoose Promise is deprecated; cast it to global.Promise
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/time-line', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const server = express();
-
-// initialize firebase for push notifications
-// TODO figure out what is up with data base url
-// in google they had it as'https://<DATABASE_NAME>.firebaseio.com'
-// is this not necessary tho since we just need for FCM not using
-// FB as actual database?
-// https://firebase.google.com/docs/admin/setup#add_the_sdk
-// also is best place for all this code here or somewhere else?
-// admin.initializeApp({
-//     credential: admin.credential.applicationDefault()
-//   });
-
-//module.admin = admin;
 
 // parsing middleware
 const jsonParser = bodyParser.json();
@@ -46,5 +32,3 @@ docs(server, mongoose);
 server.listen(4000, function () {
     console.log('API is up');
 });
-
-//module.exports = admin;
