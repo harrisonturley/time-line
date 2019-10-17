2d7553528348ad43c7f9226e09782a88362bddd6
Victoria Provenzano <victoriaprovenzano@gmail.com>
2019-10-17 13:23:27 -0700

    implemented push notification logic, still need to test push notification, and find proper spot for api key

--- API/service/searchService.js
+++ API/service/searchService.js
@@ -1,6 +1,7 @@
 const request = require('request-promise');
-const keyModule = require('../api_keys/yelpApiKey');
-const API_KEY = keyModule.API_KEY;
+//const keyModule = require('../api_keys/yelpApiKey');
+//const API_KEY = keyModule.API_KEY;
+const API_KEY = '';
 const lineupService = require('../service/lineupService');
 
 function getRestaurantsByKeywordAndCoordinates(keyword, coordinates) {
@@ -19,6 +20,22 @@ function getRestaurantsByKeywordAndCoordinates(keyword, coordinates) {
     };
 
     return request(options).then(addLineupTimes);
+}
+
+function getRestaurantsById(id) {
+    console.log("in here hello");
+    const options = {
+        uri: 'https://api.yelp.com/v3/businesses/' + id,
+        headers: {
+            'Authorization': 'Bearer ' + API_KEY
+        },
+        json: true
+    };
+    // find out how to extract name from here
+    return request(options).then(function (searchResults) {
+        console.log(searchResults.name);
+        return searchResults.name;
+    });
 }
 
 /**
@@ -58,4 +75,4 @@ function addLineupTimes(searchResults) {
     });
 }
 
-module.exports = {getRestaurantsByKeywordAndCoordinates};
+module.exports = {getRestaurantsByKeywordAndCoordinates, getRestaurantsById};
