c2accf994fcc874c9c115225296d417a33538e78
Harrison Turley <harrison.turley.1998@gmail.com>
2019-10-17 13:32:42 -0700

    Implement notification service

--- Android/app/src/main/java/com/cpen321/cloutservices/timeline/SearchActivity.java
+++ Android/app/src/main/java/com/cpen321/cloutservices/timeline/SearchActivity.java
@@ -1,12 +1,25 @@
 package com.cpen321.cloutservices.timeline;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.cpen321.cloutservices.timeline.model.Notification;
+import com.cpen321.cloutservices.timeline.model.NotificationService;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 
 public class SearchActivity extends AppCompatActivity {
@@ -34,6 +47,37 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     public void onClickTestButton(View v) {
+        FirebaseInstanceId.getInstance().getInstanceId()
+            .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                @Override
+                public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                    if (!task.isSuccessful()) {
+                        Log.w("GET_INSTANCE_ID", "getInstanceId failed", task.getException());
+                        return;
+                    }
 
+                    // Get new Instance ID token
+                    String token = task.getResult().getToken();
+
+                    // Log and toast
+                    Log.d("GET_INSTANCE_ID", token);
+                    Toast.makeText(SearchActivity.this, token, Toast.LENGTH_SHORT).show();
+
+                    Notification notification = new Notification(token);
+                    NotificationService notificationSub = RetrofitClientInstance.getRetrofitInstance().create(NotificationService.class);
+                    Call<Notification> call = notificationSub.sendNotificationToken(notification);
+                    call.enqueue(new Callback<Notification>() {
+                        @Override
+                        public void onResponse(Call<Notification> call, Response<Notification> response) {
+                            Toast.makeText(SearchActivity.this, "Call sent", Toast.LENGTH_LONG).show();
+                        }
+
+                        @Override
+                        public void onFailure(Call<Notification> call, Throwable t) {
+                            Toast.makeText(SearchActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
     }
 }
--- Android/app/src/main/java/com/cpen321/cloutservices/timeline/model/Notification.java
+++ Android/app/src/main/java/com/cpen321/cloutservices/timeline/model/Notification.java
@@ -1,4 +1,21 @@
 package com.cpen321.cloutservices.timeline.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Notification {
+
+    @SerializedName("Token")
+    private String token;
+
+    public Notification(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
