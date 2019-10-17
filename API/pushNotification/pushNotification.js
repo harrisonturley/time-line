6118dce31eb7243ff24c62bf77fc6a8c78373100
Victoria Provenzano <victoriaprovenzano@gmail.com>
2019-10-17 16:49:35 -0700

    push notification logic should now work if anyone updates a lineup to be under 5 minutes (only works for lineups whose id is  a real yelp id

--- API/pushNotification/pushNotification.js
+++ API/pushNotification/pushNotification.js
@@ -5,34 +5,31 @@ const searchService = require('../service/searchService');
 // everyone should already be subscribed to this topic
 var topic = 'globalTopic';
 
-var message = {
-  data: {
-    score: '850',
-    time: '2:45'
-  },
-  topic: topic
-};
-
 function checkToSendPushNotification(body, id) {
-  //need to get restaurant name from id
-  //un hardcode this
-  console.log(admin);
   
   searchService.getRestaurantsById(id).then(function (name) {
+
     if(body.lineupTime<=5){
-      console.log("would send message about restaurant:" + name);
+
+      var message = {
+        notification: {
+          body: name + ' has under a 5 minute wait time!',
+          title: 'Notification from Time Line'
+        },
+        topic: topic
+      };
+      
       admin.messaging().send(message)
       .then((response) => {
         // Response is a message ID string.
         console.log('Successfully sent message:', response);
-        res.status(200).send();
       })
       .catch((error) => {
         console.log('Error sending message:', error);
-        res.status(422).send({error: err.message});
       });
+
     }
   });
-}
+};
 
 module.exports = {checkToSendPushNotification};
