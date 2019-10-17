32ff6c401da6e72f83611450735b7791786e8e8f
Harrison Turley <harrison.turley.1998@gmail.com>
2019-10-17 01:23:19 -0700

    Add hamburger menu

--- Android/app/src/main/java/com/cpen321/cloutservices/timeline/SearchActivity.java
+++ Android/app/src/main/java/com/cpen321/cloutservices/timeline/SearchActivity.java
@@ -1,8 +1,12 @@
 package com.cpen321.cloutservices.timeline;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Toolbar t = findViewById(R.id.toolbar_main);
+        setSupportActionBar(t);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, t, R.string.app_name, R.string.app_name);
+        drawer.addDrawerListener(toggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 }
