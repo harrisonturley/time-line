2c66977ddaefa2f3e27bb0faaaf420a2cd1cd7a1
Harrison Turley <harrison.turley.1998@gmail.com>
2019-10-19 22:55:36 -0700

    Implement navigation drawer

--- Android/app/src/main/java/com/cpen321/cloutservices/timeline/SearchFragment.java
+++ Android/app/src/main/java/com/cpen321/cloutservices/timeline/SearchFragment.java
@@ -64,7 +64,6 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         return view;
     }
@@ -72,11 +71,6 @@ public class SearchFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-
     }
 
     @Override
