04d7a7cfbb0daca4296c207c0b1130e0222bcc9f
Victoria Provenzano <victoriaprovenzano@gmail.com>
2019-10-20 08:53:13 -0700

    fixed circular dependency from push notification

--- API/pushNotification/pushNotification.js
+++ API/pushNotification/pushNotification.js
@@ -1,13 +1,14 @@
 var firebaseAdmin = require('./notificationController');
 var admin = firebaseAdmin.admin;
-const searchService = require('../service/searchService');
+const API_KEY = 'rw0fMRw0_c05_ankeAlpIBhpuejV80QfLKT8Ktx7Mywhj8gw1R8a8_sqmYYvt2HBvaXus2kB7xrwiWreoSXHtNqW0ASxeM4GVsWEfZKaYNI9JT7IrmGBa4owV8WoXXYx';
+const request = require('request-promise');
 
 // everyone should already be subscribed to this topic
 var topic = 'globalTopic';
 
 function checkToSendPushNotification(body, id) {
   
-  searchService.getRestaurantsById(id).then(function (name) {
+  getRestaurantsById(id).then(function (name) {
 
     if(body.lineupTime<=5){
 
@@ -32,4 +33,18 @@ function checkToSendPushNotification(body, id) {
   });
 };
 
-module.exports = {checkToSendPushNotification};
+function getRestaurantsById(id) {
+  const options = {
+      uri: 'https://api.yelp.com/v3/businesses/' + id,
+      headers: {
+          'Authorization': 'Bearer ' + API_KEY
+      },
+      json: true
+  };
+  
+  return request(options).then(function (searchResults) {
+      return searchResults.name;
+  });
+}
+
+module.exports = {checkToSendPushNotification, getRestaurantsById};
