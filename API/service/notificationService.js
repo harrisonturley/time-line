ad30f27895b50bf97af9a1b55963d04010645cb7
Victoria Provenzano <victoriaprovenzano@gmail.com>
2019-10-20 14:05:52 -0700

    cleaned up code and tested it

--- API/service/notificationService.js
+++ API/service/notificationService.js
@@ -1,4 +1,4 @@
-var firebaseAdmin = require('./notificationController');
+var firebaseAdmin = require('../controller/notificationController');
 var admin = firebaseAdmin.admin;
 const API_KEY = 'rw0fMRw0_c05_ankeAlpIBhpuejV80QfLKT8Ktx7Mywhj8gw1R8a8_sqmYYvt2HBvaXus2kB7xrwiWreoSXHtNqW0ASxeM4GVsWEfZKaYNI9JT7IrmGBa4owV8WoXXYx';
 const request = require('request-promise');
@@ -6,11 +6,13 @@ const request = require('request-promise');
 // everyone should already be subscribed to this topic
 var topic = 'globalTopic';
 
+// please note that this will throw an error
+// if it is not given a valid yelp restaurant id
 function checkToSendPushNotification(body, id) {
   
   getRestaurantsById(id).then(function (name) {
 
-    if(body.lineupTime<=5){
+    if(body.lineupTime <= 5){
 
       var message = {
         notification: {
@@ -28,11 +30,16 @@ function checkToSendPushNotification(body, id) {
       .catch((error) => {
         console.log('Error sending message:', error);
       });
-
     }
+    else{
+      console.log('No need for push notification.');
+    }
+  }).catch((error) => {
+    console.log('Not a valid restaurant ID, no notification sent');
   });
 };
 
+// this also requires a valid yelp id
 function getRestaurantsById(id) {
   const options = {
       uri: 'https://api.yelp.com/v3/businesses/' + id,
