54a2117f6bf8b24e87e3f1670294af3e29a7365d
Harrison Turley <harrison.turley.1998@gmail.com>
2019-10-20 21:08:01 -0700

    Add initial functionality for Search

---
+++ Android/app/src/main/java/com/cpen321/cloutservices/timeline/model/Businesses.java
@@ -0,0 +1,14 @@
+package com.cpen321.cloutservices.timeline.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Businesses {
+
+    private List<Restaurant> businesses;
+
+    public List<Restaurant> getBusinesses() {
+        return businesses;
+    }
+}
