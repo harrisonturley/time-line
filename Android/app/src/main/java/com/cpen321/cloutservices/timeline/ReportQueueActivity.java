de11cee51ae1b5138e49d186d8571a40bbc9f6a2
khyjoon <khyunjoonpeterkim@gmail.com>
2019-10-21 03:13:00 -0700

    ReportQUI finished basically, CHANGED backend of lineup a little

--- Android/app/src/main/java/com/cpen321/cloutservices/timeline/ReportQueueActivity.java
+++ Android/app/src/main/java/com/cpen321/cloutservices/timeline/ReportQueueActivity.java
@@ -51,12 +51,59 @@ public class ReportQueueActivity extends AppCompatActivity {
             public void onClick(View v) {
                 lineuptime = Integer.parseInt(queueInput.getText().toString());
                 // send info to backend api call lets go
-                postLineupTime(restaurantId, lineuptime);
+                putLineupTime(restaurantId, lineuptime);
             }
         });
     }
 
+
     // method to post the line up time
+    public void putLineupTime(String restaurantId, Integer lineuptime) {
+        // setup progess bar
+        progressBar.setVisibility(View.VISIBLE);
+        Log.wtf("Print restaurantId: ", restaurantId);
+        Log.wtf("Print lineuptime: ", lineuptime.toString());
+
+        Lineup lineup = new Lineup();
+        lineup.setLineupTime(lineuptime);
+
+        LineupService service = RetrofitClientInstance.getRetrofitInstance().create(LineupService.class);
+        Call<Lineup> call = service.updateLineup(restaurantId, lineup);
+
+        call.enqueue(new Callback<Lineup>() {
+            @Override
+            public void onResponse(Call<Lineup> call, Response<Lineup> response) {
+                // wtf is going on
+                Log.wtf("Call request", call.request().toString());
+                Log.wtf("Call request header", call.request().headers().toString());
+                Log.wtf("Response raw header", response.headers().toString());
+                Log.wtf("Response raw", String.valueOf(response.raw().body()));
+                Log.wtf("Response code", String.valueOf(response.code()));
+
+                //  object does not exist yet
+                if (response.isSuccessful()) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(ReportQueueActivity.this, "You have submitted your lineup time", Toast.LENGTH_LONG).show();
+
+                    // if not successful, object exists already. Update instead!
+                } else {
+                    System.out.println("ERROR "+response.raw().body());
+                    Log.wtf("Response errorBody", String.valueOf(response.errorBody()));
+                    Toast.makeText(ReportQueueActivity.this,  "Cannot submit new lineup time while submission is in progress", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Lineup> call, Throwable t) {
+                Toast.makeText(ReportQueueActivity.this, "Failed to submit. Check your Internet connection", Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.INVISIBLE);
+                Log.wtf("Error", t.getMessage());
+            }
+        });
+    }
+/*
+    // method to post the line up time -- not used anymore... kept for nostalgia
     public void postLineupTime(String restaurantId, Integer lineuptime) {
         // setup progess bar
         progressBar.setVisibility(View.VISIBLE);
@@ -70,21 +117,23 @@ public class ReportQueueActivity extends AppCompatActivity {
         call.enqueue(new Callback<Lineup>() {
             @Override
             public void onResponse(Call<Lineup> call, Response<Lineup> response) {
+                // wtf is going on
                 Log.wtf("Call request", call.request().toString());
                 Log.wtf("Call request header", call.request().headers().toString());
                 Log.wtf("Response raw header", response.headers().toString());
                 Log.wtf("Response raw", String.valueOf(response.raw().body()));
                 Log.wtf("Response code", String.valueOf(response.code()));
 
+                //  object does not exist yet
                 if (response.isSuccessful()) {
                     progressBar.setVisibility(View.INVISIBLE);
                     Toast.makeText(ReportQueueActivity.this, "You have submitted your lineup time", Toast.LENGTH_LONG).show();
 
-
+                // if not successful, object exists already. Update instead!
                 } else {
                     System.out.println("ERROR "+response.raw().body());
                     Log.wtf("Response errorBody", String.valueOf(response.errorBody()));
-                    Toast.makeText(ReportQueueActivity.this,  "Cannot submit new lineup time while your previous time is in progress.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(ReportQueueActivity.this,  "Cannot submit new lineup time while submission is in progress", Toast.LENGTH_LONG).show();
                     progressBar.setVisibility(View.INVISIBLE);
                 }
             }
@@ -97,6 +146,6 @@ public class ReportQueueActivity extends AppCompatActivity {
             }
         });
     }
-
-/* end of class */
+*/
+    /* end of class */
 }
