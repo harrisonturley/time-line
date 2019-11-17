3d86de4fab86b87daf7d4e88f67f5dedb7331aa7
Mark Pang <30737313+Pairallax@users.noreply.github.com>
2019-11-16 18:23:36 -0800

    Add user seeds

---
+++ API/tests/seeds/user.seed.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        email: "alice@test.com",
+        name: "Alice",
+        balance: 0
+    },
+    {
+        email: "bob@test.com",
+        name: "Bob",
+        balance: 100
+    },
+    {
+        email: "chen@test.com",
+        name: "Chen",
+        balance: 200
+    }
+];
