01395323b864527e987a2fdc6e23239401e117ce
Mark Pang <30737313+Pairallax@users.noreply.github.com>
2019-11-16 17:55:05 -0800

    Add lineup seeds

---
+++ API/tests/seeds/lineup.seed.js
@@ -0,0 +1,16 @@
+const uuid = require('uuid');
+
+module.exports = [
+    {
+        id: uuid.v1(),
+        lineupTime: 10
+    },
+    {
+        id: uuid.v1(),
+        lineupTime: 20
+    },
+    {
+        id: uuid.v1(),
+        lineupTime: 30
+    }
+];
