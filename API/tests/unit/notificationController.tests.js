e9b0c2ba3728066cc429c2fca41159796414ec55
Victoria Provenzano <victoriaprovenzano@gmail.com>
2019-11-18 22:59:20 -0800

    mocked notification controller, passes test

---
+++ API/tests/unit/notificationController.tests.js
@@ -0,0 +1,25 @@
+const request = require("supertest");
+const app = require("../../server.js");
+var admin = jest.genMockFromModule("firebase-admin").default;
+admin.initializaApp = jest.fn(secret => secret === 'not wizard');
+
+
+describe("Notification Controller Unit", () => {
+    it("Post OK", async () => {
+      const res = await request(app)
+        .post("/api/notification/subscribe")
+        .send({
+          registrationToken: "this test is cool"
+        })
+      expect(res.statusCode).toEqual(200);
+    })
+  
+    it("Post ERR", async () => {
+      const res = await request(app)
+        .post("/api/notification/subscribe")
+        .send({
+          badToken: "badToken"
+        })
+      expect(res.statusCode).toEqual(422);
+    })
+})
