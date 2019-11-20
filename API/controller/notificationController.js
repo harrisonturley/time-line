2e0cf3faf790015b83d00cf1824cb14f01f26e1d
Victoria Provenzano <victoriaprovenzano@gmail.com>
2019-11-20 13:21:31 -0800

    moving firebase admin out of the controller into the service

--- API/controller/notificationController.js
+++ API/controller/notificationController.js
@@ -1,26 +1,13 @@
 
 const express = require("express");
 const router = express.Router();
-var admin = require("firebase-admin");
-//var globalTopic = "globalTopic";
-var userService = require("../service/userService")
+const notificationService = require("../service/notificationService");
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault()
-});
-
-module.admin = admin;
-
-//subscribes users to a restaurant from restaurant id 
-//need: restaurant id for topic, and, registrationToken
 router.post("/notification/subscribe", function (req, res, next) {
     
-  admin.messaging().subscribeToTopic(req.body.registrationToken, req.body.restaurantId)
+  notificationService.subscribe(req.body.registrationToken, req.body.restaurantId)
   .then(function(response) {
-    console.log(req.body.restaurantId)
-    //userService.updateUser(req.body.email, { $push: { favoriteRestaurants: req.body.restaurantId} });
-    //userService.updateUser(req.body.email, { name: "Bob" });
-    console.log("Successfully subscribed to topic:");
+    console.log("Successfully subscribed to topic");
     res.status(200).send();
   })
   .catch(function(error) {
@@ -31,23 +18,17 @@ router.post("/notification/subscribe", function (req, res, next) {
 
 router.post("/notification/unsubscribe", function (req, res, next) {
     
-  admin.messaging().unsubscribeFromTopic(req.body.registrationToken, req.body.restaurantId)
+  notificationService.unsubscribe(req.body.registrationToken, req.body.restaurantId)
   .then(function(response) {
-    console.log("Successfully subscribed to topic:");
-    //userService.updateUser(req.body.email, { $pull: { favoriteRestaurants: req.body.restaurantId} })
+    console.log("Successfully unsubscribed from topic");
     res.status(200).send();
   })
   .catch(function(error) {
-    console.log("Error subscribing to topic:");
+    console.log("Error unsubscribing from topic:"+ error);
     res.status(422).send({error: error.message});
   });
 });
 
-
-//get all favorited
-//given user id, returns list
-
 module.exports = router;
-module.exports.admin = admin;
 
   
