52e248659ed50e051fec6cdd89a9cf866e79a1fa
Victoria Provenzano <victoriaprovenzano@gmail.com>
2019-11-20 12:28:43 -0800

    implementing restaurant specific subscribe and unsubscribe

--- API/controller/notificationController.js
+++ API/controller/notificationController.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const router = express.Router();
 var admin = require("firebase-admin");
-var globalTopic = "globalTopic";
+//var globalTopic = "globalTopic";
+var userService = require("../service/userService")
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault()
@@ -10,14 +11,30 @@ admin.initializeApp({
 
 module.admin = admin;
 
-// subscribes users to a general topic
+//subscribes users to a restaurant from restaurant id 
+//need: restaurant id for topic, and, registrationToken
 router.post("/notification/subscribe", function (req, res, next) {
     
-  admin.messaging().subscribeToTopic(req.body.registrationToken, globalTopic)
+  admin.messaging().subscribeToTopic(req.body.registrationToken, req.body.restaurantId)
   .then(function(response) {
-  // See the MessagingTopicManagementResponse reference documentation
-  // for the contents of response.
+    console.log(req.body.restaurantId)
+    //userService.updateUser(req.body.email, { $push: { favoriteRestaurants: req.body.restaurantId} });
+    //userService.updateUser(req.body.email, { name: "Bob" });
     console.log("Successfully subscribed to topic:");
+    res.status(200).send();
+  })
+  .catch(function(error) {
+    console.log("Error subscribing to topic:"+ error);
+    res.status(422).send({error: error.message});
+  });
+});
+
+router.post("/notification/unsubscribe", function (req, res, next) {
+    
+  admin.messaging().unsubscribeFromTopic(req.body.registrationToken, req.body.restaurantId)
+  .then(function(response) {
+    console.log("Successfully subscribed to topic:");
+    //userService.updateUser(req.body.email, { $pull: { favoriteRestaurants: req.body.restaurantId} })
     res.status(200).send();
   })
   .catch(function(error) {
@@ -25,6 +42,10 @@ router.post("/notification/subscribe", function (req, res, next) {
     res.status(422).send({error: error.message});
   });
 });
+
+
+//get all favorited
+//given user id, returns list
 
 module.exports = router;
 module.exports.admin = admin;
