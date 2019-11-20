9e2838041488846b295a69954124c056f67f9ebf
Mark Pang <30737313+Pairallax@users.noreply.github.com>
2019-11-19 22:38:13 -0800

    Refactor userFavoritesService.js

--- API/service/user/userFavoritesService.js
+++ API/service/user/userFavoritesService.js
@@ -9,11 +9,7 @@ function addUserFavorite(email, favorite) {
     return getUserFavorites(email).then(
         function (favorites) {
             favorites.favorites.push(favorite);
-            const update = {$set: favorites};
-            return User.findOneAndUpdate({email}, update, {
-                projection: {_id: 0, favorites: 1},
-                new: true
-            }).lean();
+            return updateUserFavorites(email, favorites);
         }
     );
 }
@@ -22,12 +18,16 @@ function deleteUserFavorite(email, favorite) {
     return getUserFavorites(email).then(
         function (favorites) {
             utils.removeFromArray(favorites.favorites, favorite);
-            const update = {$set: favorites};
-            return User.findOneAndUpdate({email}, update, {
-                projection: {_id: 0, favorites: 1}, new: true
-            }).lean();
+            return updateUserFavorites(email, favorites);
         }
     );
+}
+
+function updateUserFavorites(email, favorites) {
+    const update = {$set: favorites};
+    return User.findOneAndUpdate({email}, update, {
+        projection: {_id: 0, favorites: 1}, new: true
+    }).lean();
 }
 
 module.exports = {getUserFavorites, addUserFavorite, deleteUserFavorite};
