9ac9523cd845f8e3a8a83a6edb5031e0869a7f7f
Mark Pang <30737313+Pairallax@users.noreply.github.com>
2019-11-20 11:42:16 -0800

    Add ITs for getting favorite restaurants flow

--- API/tests/IT/user/userFavoritesControllerIT.test.js
+++ API/tests/IT/user/userFavoritesControllerIT.test.js
@@ -4,7 +4,9 @@ const request = supertest(app);
 const {setupDB} = require('../test-setup');
 const databaseName = 'userFavoritesControllerITDB';
 const User = require('../../../repository/user');
+const FavoritedRestaurant = require('../../../repository/favoritedRestaurant');
 const userSeeds = require('../seeds/user.seed.js');
+const favoritedRestaurantSeeds = require('../seeds/favoritedRestaurant.seed');
 const uuid = require('uuid');
 const utils = require("../../../util/collections");
 
@@ -31,12 +33,50 @@ describe("userFavoritesController", () => {
         const seededUser = userSeeds[0];
         const email = seededUser.email;
         const favorites = seededUser.favorites;
-        const newFavorite = uuid.v1();
-        favorites.push(newFavorite);
+        const newFavoriteRestaurant = {
+            id: 'vtGxOG7DVS7ftBzRW94uDA',
+            alias: 'super-chef-grill-vancouver',
+            name: 'Super Chef Grill',
+            image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/Z7UPEekmVioWdtzRSMNIGA/o.jpg',
+            is_closed: false,
+            url: 'https://www.yelp.com/biz/super-chef-grill-vancouver?adjust_creative=2ojO4ZImtGy71cMqoKgOQw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=2ojO4ZImtGy71cMqoKgOQw',
+            review_count: 68,
+            categories: [
+                {
+                    alias: 'chinese',
+                    title: 'Chinese'
+                }
+            ],
+            rating: 4.5,
+            coordinates: {
+                latitude: 49.279012421188,
+                longitude: -123.116581099631
+            },
+            transactions: [],
+            price: '$',
+            location: {
+                address1: '280 Robson Street',
+                address2: '',
+                address3: '',
+                city: 'Vancouver',
+                zip_code: 'V6B 0E7',
+                country: 'CA',
+                state: 'BC',
+                display_address: [
+                    '280 Robson Street',
+                    'Vancouver, BC V6B 0E7',
+                    'Canada'
+                ]
+            },
+            phone: '+16048282083',
+            display_phone: '+1 604-828-2083',
+            distance: 507.3463646289611,
+        };
+        favorites.push(newFavoriteRestaurant.id);
 
         const res = await request.post('/api/users/' + email + '/favorites')
             .send({
-                favorite: newFavorite
+                restaurant: newFavoriteRestaurant
             });
 
         expect(res.status).toBe(200);
@@ -46,6 +86,11 @@ describe("userFavoritesController", () => {
         expect(res.body).not.toHaveProperty('balance');
         const user = await User.findOne({email: email}).lean();
         expect(user.favorites).toEqual(favorites);
+        const favoritedRestaurant = await FavoritedRestaurant.findOne(
+            {id: newFavoriteRestaurant.id},
+            {_id: 0, __v: 0}
+        ).lean();
+        expect(favoritedRestaurant).toEqual(newFavoriteRestaurant);
         done();
     });
 
@@ -53,7 +98,7 @@ describe("userFavoritesController", () => {
         const seededUser = userSeeds[2];
         const email = seededUser.email;
         const favorites = seededUser.favorites;
-        const favoriteToDelete = favorites[1];
+        const favoriteToDelete = favorites[0];
         utils.removeFromArray(favorites, favoriteToDelete);
 
         const res = await request.delete('/api/users/' + email + '/favorites/' + favoriteToDelete);
@@ -65,6 +110,11 @@ describe("userFavoritesController", () => {
         expect(res.body).not.toHaveProperty('balance');
         const userFavorites = await User.findOne({email}, {_id: 0, favorites: 1}).lean();
         expect(userFavorites.favorites).toEqual(favorites);
+        const favoritedRestaurantExists = await FavoritedRestaurant.exists(
+            {id: favoriteToDelete},
+            {_id: 0, __v: 0}
+        );
+        expect(favoritedRestaurantExists).toBe(true);
         done();
     });
 });
