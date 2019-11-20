2002204e28109f12fbb9947a4efcacdaa652b02f
Mark Pang <30737313+Pairallax@users.noreply.github.com>
2019-11-20 13:39:31 -0800

    Integrate subscription logic with favorites flow

--- API/service/user/userFavoritesService.js
+++ API/service/user/userFavoritesService.js
@@ -1,12 +1,14 @@
 const User = require("../../repository/user");
 const utils = require("../../util/collections");
 const favoritedRestaurantService = require("../favoritedRestaurantService");
+const notificationService = require("../../service/notificationService");
 
 function getUserFavorites(email) {
     return User.findOne({email}, {_id: 0, favorites: 1}).lean();
 }
 
-async function addUserFavorite(email, restaurant) {
+async function addUserFavorite(email, restaurant, registrationToken) {
+    notificationService.subscribe(registrationToken, restaurant.id);
     const favorites = await getUserFavorites(email);
     favorites.favorites.push(restaurant.id);
     await favoritedRestaurantService.addFavoritedRestaurant(restaurant);
@@ -14,12 +16,14 @@ async function addUserFavorite(email, restaurant) {
 }
 
 async function deleteUserFavorite(email, restaurantId) {
+    notificationService.unsubscribe(registrationToken, restaurantId);
     const favorites = await getUserFavorites(email);
     utils.removeFromArray(favorites.favorites, restaurantId);
     // await favoritedRestaurantService.deleteFavoritedRestaurant(restaurantId);
     return updateUserFavorites(email, favorites);
 }
 
+// private
 function updateUserFavorites(email, favorites) {
     const update = {$set: favorites};
     return User.findOneAndUpdate({email}, update, {
