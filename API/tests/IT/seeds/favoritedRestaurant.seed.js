eec1d6601f861f5a64b416f2b9d354821ba0526c
Mark Pang <30737313+Pairallax@users.noreply.github.com>
2019-11-20 12:42:06 -0800

    Refactor favoritedRestaurant model and response from search endpoint

--- API/tests/IT/seeds/favoritedRestaurant.seed.js
+++ API/tests/IT/seeds/favoritedRestaurant.seed.js
@@ -1,66 +1,35 @@
 module.exports = [
     {
         id: 'vnKoBdTuh2lsUKASMwQYbA',
-        alias: 'chongqing-vancouver-3',
         name: 'ChongQing',
         image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/jn99Vkgi4Gj3gxQAdWs29A/o.jpg',
         is_closed: false,
-        url: 'https://www.yelp.com/biz/chongqing-vancouver-3?adjust_creative=2ojO4ZImtGy71cMqoKgOQw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=2ojO4ZImtGy71cMqoKgOQw',
-        review_count: 342,
-        categories: [
-            {
-                alias: 'chinese',
-                title: 'Chinese'
-            }
-        ],
         rating: 4,
         coordinates: {
             latitude: 49.286526424773,
             longitude: -123.128171776697
         },
-        transactions: [],
-        price: '$$',
         location: {
-            address1: '1260 Robson Street',
-            address2: '',
-            address3: '',
-            city: 'Vancouver',
-            zip_code: 'V6E 1C1',
-            country: 'CA',
-            state: 'BC',
             display_address: [
                 '1260 Robson Street',
                 'Vancouver, BC V6E 1C1',
                 'Canada'
             ]
         },
-        phone: '+16045680303',
-        display_phone: '+1 604-568-0303',
         distance: 689.0129820407582,
         lineupTime: 50,
         timestamp: 1574278529
     },
     {
         id: 'JgSGpSMHbGecAXs_o1rE_g',
-        alias: 'dinesty-dumpling-house-vancouver',
         name: 'Dinesty Dumpling House',
         image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/aAGd_yS6fBTn2s6tnb4wlA/o.jpg',
         is_closed: false,
-        url: 'https://www.yelp.com/biz/dinesty-dumpling-house-vancouver?adjust_creative=2ojO4ZImtGy71cMqoKgOQw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=2ojO4ZImtGy71cMqoKgOQw',
-        review_count: 741,
-        categories: [
-            {
-                alias: 'chinese',
-                title: 'Chinese'
-            }
-        ],
         rating: 4,
         coordinates: {
             latitude: 49.2908176,
             longitude: -123.1343912
         },
-        transactions: [],
-        price: '$$',
         location: {
             address1: '1719 Robson Street',
             address2: '',
@@ -75,49 +44,27 @@ module.exports = [
                 'Canada'
             ]
         },
-        phone: '+16046697769',
-        display_phone: '+1 604-669-7769',
         distance: 1341.9495433736781,
         lineupTime: null,
         timestamp: 1574278639
     },
     {
         id: 'K1943yeGQELTUeiH6bDa2g',
-        alias: 'bao-bei-chinese-brasserie-vancouver',
         name: 'Bao Bei Chinese Brasserie',
         image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/QmAy8YcOum-tJGTV6a_JKA/o.jpg',
         is_closed: false,
-        url: 'https://www.yelp.com/biz/bao-bei-chinese-brasserie-vancouver?adjust_creative=2ojO4ZImtGy71cMqoKgOQw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=2ojO4ZImtGy71cMqoKgOQw',
-        review_count: 363,
-        categories: [
-            {
-                alias: 'chinese',
-                title: 'Chinese'
-            }
-        ],
         rating: 4,
         coordinates: {
             latitude: 49.2796095312486,
             longitude: -123.100574596039
         },
-        transactions: [],
-        price: '$$$',
         location: {
-            address1: '163 Keefer Street',
-            address2: '',
-            address3: '',
-            city: 'Vancouver',
-            zip_code: 'V6A 1X3',
-            country: 'CA',
-            state: 'BC',
             display_address: [
                 '163 Keefer Street',
                 'Vancouver, BC V6A 1X3',
                 'Canada'
             ]
         },
-        phone: '+16046880876',
-        display_phone: '+1 604-688-0876',
         distance: 1499.7511186780425,
         lineupTime: 10,
         timestamp: 1574378529
